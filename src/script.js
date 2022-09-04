5175a3dc2b4adce68c4ffe5c613092be65c8498b
Lone1106 <97834596+Lone1106@users.noreply.github.com>
2022-09-04 12:18:29 +0200

    Added new element functions, new switch routes, new element styling, fixed old element styling

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 const textContainter = document.getElementById("terminal__main");
 const area = document.getElementById("area");
 const year = document.querySelector(".year");
+const container = document.querySelector(".container");
+
+const lineDelay = 150;
+let started = false;
 
 // SET STUFF ON LOAD
 window.onload = () => {
@@ -9,8 +13,18 @@ window.onload = () => {
 };
 
 // EVENT LISTENERS
+container.addEventListener("click", () => {
+	area.focus();
+});
+
 document.addEventListener("keydown", (e) => {
 	if (e.key === "Enter") {
+		// DELETE INFO LINE AFTER FIRST COMMAND
+		if (!started) {
+			resetTerminal();
+			started = true;
+		}
+
 		let data = getInput();
 		actOnInput(data);
 		clearInput();
@@ -30,21 +44,13 @@ function getInput() {
 	return data;
 }
 
+// CLEAR FUNCTIONS
 async function clearInput() {
 	const label = document.querySelector(".cursor");
-	await delay(150);
+	await delay(50);
 	area.value = "";
 	label.innerHTML = "";
 }
-
-function createText(text) {
-	const field = document.getElementById("textfield");
-	const p = document.createElement("p");
-	p.setAttribute("class", "linespace");
-	p.innerHTML = text;
-	field.appendChild(p);
-}
-
 function clearChilds() {
 	textContainter.innerHTML = "";
 }
@@ -53,6 +59,15 @@ function resetTerminal() {
 	textContainter.innerHTML = "";
 	addTextField();
 	createInputLine();
+}
+
+// CREATE ELEMENT FUNCTIONS
+function createText(text) {
+	const field = document.getElementById("textfield");
+	const p = document.createElement("p");
+	p.setAttribute("class", "linespace");
+	p.innerHTML = text;
+	field.appendChild(p);
 }
 
 function newLine() {
@@ -95,32 +110,139 @@ function addTextField() {
 	textContainter.appendChild(field);
 }
 
+function createLink(text, link) {
+	const field = document.getElementById("textfield");
+	const p = document.createElement("p");
+	const span = document.createElement("span");
+	const a = document.createElement("a");
+	p.setAttribute("class", "linespace");
+	a.setAttribute("href", link);
+	a.setAttribute("class", "socialLink");
+	a.setAttribute("target", "_blank");
+	a.innerHTML = text;
+	a.appendChild(span);
+	p.appendChild(a);
+	field.appendChild(p);
+}
+
+function createInfoLine(type, text) {
+	const field = document.getElementById("textfield");
+	const p = document.createElement("p");
+	const span1 = document.createElement("span");
+	const span2 = document.createElement("span");
+	const span3 = document.createElement("span");
+	p.setAttribute("class", "linespace");
+	span1.setAttribute("class", "infoType");
+	span2.setAttribute("class", "infoText");
+	span3.setAttribute("class", "spaceholder");
+	span1.innerHTML = type;
+	span2.innerHTML = text;
+	span3.innerHTML = "~";
+	p.appendChild(span1);
+	p.appendChild(span3);
+	p.appendChild(span2);
+	field.appendChild(p);
+}
+
+function createHeader(text) {
+	const field = document.getElementById("textfield");
+	const h3 = document.createElement("h3");
+	h3.setAttribute("class", "linespace");
+	h3.innerHTML = text;
+	field.appendChild(h3);
+}
+// CREATE SECTIONS
 async function startup() {
 	addTextField();
-	await delay(300);
+	await delay(lineDelay);
 	createText("Connecting to server. . .");
-	await delay(300);
+	await delay(lineDelay);
 	createText("Loading database. . . ");
-	await delay(300);
+	await delay(lineDelay);
 	clearChilds();
 	addTextField();
-	await delay(300);
-	createText("type help for all commands");
-	await delay(300);
+	await delay(lineDelay);
+	createText("type 'help' for all commands");
+	await delay(lineDelay);
 	createInputLine();
+}
+
+async function help() {
+	await delay(lineDelay);
+	createHeader("Commands");
+	await delay(lineDelay);
+	createInfoLine("help", "display all available commands");
+	await delay(lineDelay);
+	createInfoLine("clear", "clear the console of all in- and output");
+	await delay(lineDelay);
+	createInfoLine("about", "gets info about owner of the page");
+	await delay(lineDelay);
+	createInfoLine("projects", "take a look at the owners projects");
+	await delay(lineDelay);
+	createInfoLine("links", "get links for the owners social media");
+}
+
+async function about() {
+	await delay(lineDelay);
+	createHeader("About");
+	await delay(lineDelay);
+	createInfoLine("Name", "Name goes here");
+	await delay(lineDelay);
+	createInfoLine("Location", "Location goes here");
+	await delay(lineDelay);
+	createInfoLine("Age", "21");
+	await delay(lineDelay);
+	createInfoLine("Occupation", "Job name goes here");
+	await delay(lineDelay);
+	createInfoLine("Skills", "HTML, CSS/SASS, JS, React, Python, Adobe AI/PS");
+	await delay(lineDelay);
+	createInfoLine("Languages", "German, English, Japanese, Spanish");
+	await delay(lineDelay);
+	createInfoLine("Hobbies", "Reading, coding, tennis, meeting with friends");
+}
+
+async function projects() {
+	await delay(lineDelay);
+	createHeader("Projects");
+	await delay(lineDelay);
+	createText("Terminal Portfolio website");
+	createLink("view on Github", "https://github.com/Lone1106/terminal-website");
+	createLink("live preview", "https://www.google.de/");
+	await delay(lineDelay);
+	createText("React Flashcard Appliation");
+	createLink("view on Github", "https://github.com/Lone1106/flashcard-app");
+	createLink("live preview", "https://www.google.de/");
+}
+
+async function links() {
+	await delay(lineDelay);
+	createHeader("Links");
+	await delay(lineDelay);
+	createLink("GitHub", "https://github.com/Lone1106");
+	await delay(lineDelay);
+	createLink("LinkedIn", "https://www.linkedin.com/");
 }
 
 // HANDLE INPUTS
 function actOnInput(inp) {
 	switch (inp) {
 		case "help":
-			createText("All commands: help, clear, about, projects, links");
+			help();
 			break;
 		case "clear":
 			resetTerminal();
 			break;
+		case "about":
+			about();
+			break;
+		case "projects":
+			projects();
+			break;
+		case "links":
+			links();
+			break;
 		default:
-			createError("Command not defined! Check ? for all commands!");
+			createError("Command not defined! Check 'help' for all commands!");
 			break;
 	}
 }
